1fdeaeec4073f16e893348ac13c00e205d278f35
Tobiasee16 <engedaltobias@icloud.no>
2025-12-05 12:06:45 +0100

    Ny bruker har ingen rolle

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14bc441a256d4e4066157e38ee8167aedafe570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6464951f058e4913bf143beb18c87047182dc99f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
